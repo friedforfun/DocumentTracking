b2d5c7bb3234b8f47f57d1d6334a7a1caf9e30ce
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-12 17:44:52 +0000

    initial Skeleton of views

--- Application/DocuTrace/analysis/views.py
+++ Application/DocuTrace/analysis/views.py
@@ -2,10 +2,14 @@ from .abstractClasses import Analyse
 
 class LocationViews(Analyse):
 
-    def __init__(self):
+    def __init__(self, by_country=True):
+        self.by_country = by_country
         pass
 
     def histogram(self):
+        pass
+
+    def get_data(self):
         pass
 
 
---
+++ Application/tests/analysis/test_views.py
@@ -0,0 +1,5 @@
+import pytest
+from unittest.mock import patch, mock_open
+from Application.DocuTrace.analysis.views import LocationViews, BrowserViews
+
+
