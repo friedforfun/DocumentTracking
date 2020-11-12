eaf399e7de2d6f48075c124bdae512e5c02c5bd9
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-12 16:10:38 +0000

    Added a couple more tests

--- Application/tests/analysis/test_FileRead.py
+++ Application/tests/analysis/test_FileRead.py
@@ -1,11 +1,27 @@
 from Application.DocuTrace.analysis.fileRead import stream_read_json
 import os
-# import pytest
+import pytest
 
 key_list = ['ts', 'visitor_uuid', 'visitor_username', 'visitor_source', 'visitor_device', 'visitor_useragent', 'visitor_ip', 'visitor_country',
  'visitor_referrer', 'env_type', 'env_doc_id', 'env_adid', 'event_type', 'subject_type', 'subject_doc_id', 'subject_page', 'cause_type']
 
-def test_stream_read_json():
+
+def test_iter_stream_read_json():
+    j_iter = stream_read_json(os.path.abspath('./Application/sample_data/issuu_sample.json'))
+    count = 0
+    for i in j_iter:
+        count += 1
+    assert count == 4
+    
+def test_exception_stream_read_json():
+    j_iter = stream_read_json(os.path.abspath('./Application/sample_data/issuu_sample.json'))
+    count = 0
+    for i in j_iter:
+        count += 1
+    with pytest.raises(StopIteration):
+        next(j_iter)
+
+def test_dict_stream_read_json():
     j_iter = stream_read_json(os.path.abspath('./Application/sample_data/issuu_sample.json'))
     json = next(j_iter)
     keys = list(json.keys())
