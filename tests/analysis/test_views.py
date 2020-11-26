1c91e423d90179f97f7536bff351c5f66aca81e2
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-26 22:56:45 +0000

    test_views, added new paths, modified for DataColl

--- tests/analysis/test_views.py
+++ tests/analysis/test_views.py
@@ -2,17 +2,18 @@ import pytest
 from unittest.mock import patch, mock_open
 from DocuTrace.Analysis.DataCollector import DataCollector, ReadingData
 
-mock_file_content = '{"visitor_uuid": "745409913574d4c6", "visitor_country": "MX", "event_readtime": 797, "visitor_useragent":"Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"}'
+mock_file_content = '{"visitor_uuid": "745409913574d4c6", "env_doc_id": "130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb", "visitor_country": "MX", "event_readtime": 797, "visitor_useragent":"Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"}'
 json_dict = {
             "visitor_country": "MX",
              "visitor_uuid": "745409913574d4c6",
+             "env_doc_id": "130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb",
              "event_readtime": 797,
              "visitor_useragent": "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"}
 
 
 def _setup_Views_class_():
     m = mock_open(read_data=mock_file_content)
-    with patch('DocuTrace.analysis.fileRead.open', m) as _file:
+    with patch('DocuTrace.Analysis.FileRead.open', m) as _file:
         loc_views = DataCollector(path='path')
         _file.assert_not_called()
     return loc_views
@@ -20,17 +21,17 @@ def _setup_Views_class_():
 
 def test_count():
     m = mock_open(read_data=mock_file_content)
-    with patch('DocuTrace.analysis.fileRead.open', m) as _:
+    with patch('DocuTrace.Analysis.FileRead.open', m) as _:
         loc_views = _setup_Views_class_()
         assert loc_views.counted == False
-        loc_views.count()
+        loc_views.gather_data()
         assert loc_views.counted == True
 
 
 def test_count_error():
     loc_views = DataCollector()
     with pytest.raises(AttributeError):
-        loc_views.count()
+        loc_views.gather_data()
 
 
 def test_count_countries():
@@ -55,17 +56,8 @@ def test_count_browsers():
 def test_count_user_reads():
     views = DataCollector()
     assert '745409913574d4c6' not in views.reading_data.keys()
-    views.count_read_data(json_dict)
+    views.collect_read_data(json_dict)
     assert views.reading_data['745409913574d4c6'].read_time == 797
-
-def test_country_name():
-    views = DataCollector()
-    assert views.country_name('MX') == 'Mexico'
-
-
-def test_continent_name():
-    views = DataCollector()
-    assert views.continent_name('MX') == 'North America'
 
 
 def test_top_reads():
@@ -94,3 +86,10 @@ def test_sorted():
     views.sorted(reverse=False)
     assert list(views.countries.values()) == [8, 10, 15]
 
+def test_collect_document_readers():
+    views = DataCollector()
+    assert '130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb' not in views.document_readers.keys()
+    assert '745409913574d4c6' not in views.visitor_documents.keys()
+    views.collect_document_readers(json_dict)
+    assert views.document_readers['130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb'] == ['745409913574d4c6']
+    assert views.visitor_documents['745409913574d4c6'] == ['130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb']
