8df224b689e1d89b48eee505bda15e1a17fbeeb3
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-28 01:40:54 +0000

    Debug decorator

--- Application/DocuTrace/Utils/Logging.py
+++ Application/DocuTrace/Utils/Logging.py
@@ -3,3 +3,16 @@ import sys
 
 logging.basicConfig(format='[ %(levelname)s ] %(message)s', level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger('DocuTrace')
+
+
+def debug(func):
+    """Decorator to show debug log messages
+
+    Args:
+        func (Any -> Any): Any function
+    """
+    def inner(*args, **kwargs):
+        logger.setLevel(logging.DEBUG)
+        func(*args, **kwargs)
+        logger.setLevel(logging.WARN)
+    return inner
