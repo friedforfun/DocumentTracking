b116cf0a17ef0680c7f2009ae994038538dba664
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-29 02:51:14 +0000

    CLI work

--- Application/DocuTrace/main.py
+++ Application/DocuTrace/main.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 import sys, argparse
+from threading import Thread
+from DocuTrace.Analysis.ComputeData import ComputeData
+from DocuTrace.Analysis.DataCollector import DataCollector
 from DocuTrace.Utils.Logging import logger
+from DocuTrace.Utils.Validation import is_pathname_valid, InvalidPathError
 
 def main():
-    next_step()
     run(parse_args())
 
 
 def run(args):
     try:
-        # Step 1. Parse and validate arguments
+        verbosity = args.verbose
+        logger.setLevel(verbosity)
+        logger.info('Verbosity set to display info messages.')
+        logger.debug('Verbosity set to display debug messages.')
+
+        path = validate_path(args.filepath)
+
+        data_collector = DataCollector(path)
+        load = Thread(target=process_file, args=(data_collector,), daemon=True)
+        load.start()
+
         user_uuid = args.user_uuid
         doc_uuid = args.doc_uuid
         tast_id = args.task_id
-        filename = args.filename
+
         print('Sucess!')
+        print('User_uuid: {}'.format(user_uuid))
+        print('doc_uuid: {}'.format(doc_uuid))
+        print('tast_id: {}'.format(tast_id))
         # Step 2. Load data
-        next_step(step_id=2)
+
+    except InvalidPathError as e:
+        logger.exception(e)
+        sys.exit(1)
 
     except Exception as e:
         logger.exception(e)
@@ -25,16 +44,96 @@ def run(args):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    args = parser.add_argument_group('Params')
-    args.add_argument('-u', '--user_uuid', nargs=1, help='Specifies the user uuid', required=False)
-    args.add_argument('-d', '--doc_uuid', nargs=1, help='Specifies the document uuid', required=False)
+    """Parse the args provided to this namespace
+
+    Returns:
+        ArgumentParser: Parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Command line interface for DocuTrace.')
+    args = parser.add_argument_group('Core parameters')
+    args.add_argument('-u', '--user_uuid', help='Specifies the user uuid', required=False, type=str)
+    args.add_argument('-d', '--doc_uuid',help='Specifies the document uuid', required=False, type=str)
     # Task id is mandatory for command line usage
-    args.add_argument('-t', '--task_id', nargs=1, help='Specifies the task id', required=True)
-    args.add_argument('-f', '--filepath', nargs=1, help='Specifies the file name', required=False)
-    args.add_argument('-v', '--verbose', type=str2bool, required=False, default=False, nargs='?', const=True, help='Set a verbose output')
+    args.add_argument('-t', '--task_id', help='Specifies the task id', required=True, type=int)
+    args.add_argument('-f', '--filepath', help='Specifies the file name', required=True, type=str)
+    args.add_argument('-v', '--verbose', type=int, required=False, default=30, nargs='?', const=20, help='Set the verbosity level, 20 for INFO, 10 for DEBUG')
     return parser.parse_args()
 
+
+def validate_path(path: str) -> str:
+    """Validate the path string
+
+    Args:
+        path (str): A path provided by the user
+
+    Raises:
+        InvalidPathError: Invalid path was provided too many times
+
+    Returns:
+        str: A valid path
+    """
+    tries = 0
+    if not is_pathname_valid(path) or not path.lower().endswith('.json'):
+        while (not is_pathname_valid(path) or not path.lower().endswith('.json')) and tries < 4:
+            logger.warning('Invalid path to file detected. Please check and try again. If all else fails try and absolute path.')
+            path = input('Please enter a valid path: ')
+            tries += 1
+
+        if tries > 4 or not is_pathname_valid(path):
+            raise InvalidPathError('Invalid path enterred too many times.')
+
+    return path
+
+
+def process_file(data_collector):
+    """Begin processing this file.
+    """
+    data_collector.gather_data()
+
+
+def task_1():
+    logger.info('Task 1: The core functionality of this application is written in python.')
+
+def task_2a():
+    logger.info('Task 2a. Specifies a document UUID, and return a histogram of countries of the viewers.')
+
+def task_2b():
+    logger.info('Task 2b. Group the countries by continent, and generate a histogram of the continents of the viewers.')
+
+def task_3a():
+    logger.info('Task 3a: Histogram of verbose views by browser.')
+
+def task_3b():
+    logger.info('Task 3b: Histogram of views by browser, with processed browser names.')
+
+def task_4d():
+    logger.info('Task 4d: 10 most avid readers.')
+
+def task_5():
+    logger.info('Task 5: Also likes top n documents.')
+
+def task_6():
+    logger.info('Task 6: Also likes graph, inside GUI.')
+
+def task_7():
+    logger.info('Task 7: Open the GUI')
+
+def task_8():
+    logger.info('Task 8: Command line, This is it!')
+
+
+task_picker = {
+    '1': task_1,
+    '2a': task_2a,
+    '2b': task_2b,
+    '3a': task_3a,
+    '3b': task_3b,
+    '4d': task_4d,
+    '5': task_5,
+    '6': task_6,
+    '7': task_7,
+    '8': task_8
+}
 
 def static_vars(**kwargs):
     def decorate(func):
