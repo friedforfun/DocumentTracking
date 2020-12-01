5a90cf54e545271c68cf49775fb2ef201bdf673c
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-12-01 23:09:46 +0000

    register evens in root gui

--- Application/DocuTrace/Gui/main.py
+++ Application/DocuTrace/Gui/main.py
@@ -6,20 +6,23 @@ from DocuTrace.Utils.Logging import logger
 
 class GuiRoot(tk.Tk):
 
-    def __init__(self, compute_data, doc_uuid=None, user_uuid=None, n=None):
+    def __init__(self, compute_data, doc_uuid=None, user_uuid=None, n=None, start_tab: str=None):
         super().__init__()
         self.protocol("WM_DELETE_WINDOW", self.on_close)
         self.geometry('850x750')
         #self.resizable(False, False)
         self.title('DocuTrace')
         self.tabs = ttk.Notebook(self)
-        self.tabs.bind('<<NotebookTabChanged>>', on_tab_change)
+        self.tabs.bind('<<NotebookTabChanged>>', self.on_tab_change)
         self.tabs.pack(expand=1, fill='both')
         self.tab_ref = {}
 
-        for i, (k, v) in enumerate(tab_dict.items()):
-            self.tab_ref[i] = Tab(compute_data, v, master=self.tabs, doc=doc_uuid, user=user_uuid, n=n)
-            self.tabs.add(self.tab_ref[i], text=k)
+        for k, v in tab_dict.items():
+            self.tab_ref[k] = Tab(compute_data, v, master=self.tabs, doc=doc_uuid, user=user_uuid, n=n)
+            self.tabs.add(self.tab_ref[k], text=k)
+
+        if start_tab:
+            self.tabs.select(self.tab_ref[start_tab])
 
 
     def on_close(self):
@@ -35,41 +38,18 @@ class GuiRoot(tk.Tk):
         self.mainloop()
 
 
+    def on_tab_change(self, event):
+        tab_name = event.widget.tab('current')['text']
+        logger.debug('tab: {}'.format(tab_name))
+        tab = self.tab_ref[tab_name]
+        tab.on_open()
 
 
+def open(*args, **kwargs):
 
-def on_tab_change(event):
-    tab = event.widget.tab('current')['text']
-    logger.warning('tab: {}'.format(tab))
-
-
-def open(compute_data, doc_uuid=None, user_uuid=None, n=None):
-
-    gui = GuiRoot(compute_data, doc_uuid, user_uuid, n)
+    gui = GuiRoot(*args, **kwargs)
     gui.start()
 
-
-
-    # task_2a = Tab(compute_data, task2a_widgets, master=tabs, doc=doc_uuid, n=n)
-    # tabs.add(task_2a, text='Task 2a')
-
-    # task_2b = Tab(compute_data, task2b_widgets, master=tabs, doc=doc_uuid, n=n)
-    # tabs.add(task_2b, text='Task 2b')
-
-    # task_3a = Tab(compute_data, master=tabs)
-    # tabs.add(task_3a, text='Task 3a')
-
-    # task_3b = Tab(compute_data, master=tabs)
-    # tabs.add(task_3b, text='Task 3b')
-
-    # task_4 = Tab(compute_data, master=tabs)
-    # tabs.add(task_4, text='Task 4d')
-
-    # task_5d = Tab(compute_data, master=tabs)
-    # tabs.add(task_5d, text='Task 5')
-
-    # task_6 = Tab(compute_data, master=tabs)
-    # tabs.add(task_6, text='Task 6')
 
 
     
