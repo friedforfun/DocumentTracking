2a3a04f5646f867ff77df9e8459a610725ec706a
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-12-01 23:08:44 +0000

    Finished tasks

--- Application/DocuTrace/Gui/Tasks.py
+++ Application/DocuTrace/Gui/Tasks.py
@@ -1,23 +1,5 @@
-import tkinter as tk
-from tkinter import ttk
-
-def doc_input(master, doc_stringvar, n_var, btn_fn):
-    label = ttk.Label(master, text='Document UUID: ').grid(
-        row=0, column=0, padx=10, pady=30)
-
-    input = ttk.Entry(master, textvariable=doc_stringvar)
-    input.grid(row=0, column=1, padx=5, pady=30)
-
-    button = ttk.Button(master, text='Go', command=btn_fn)
-    button.grid(row=0, column=5, padx=5, pady=30)
-
-    label_n = ttk.Label(master, text='Number to display: ').grid(
-        row=0, column=3, padx=10, pady=0)
-    input_n = ttk.Entry(master, textvariable=n_var, width=5).grid(
-        row=0, column=4, padx=10, pady=0)
-
-    return label, input, button, label_n, input_n
-
+"""Functions to help build relevant gui pages for each task
+"""
 
 def task2a_widgets(tab):
     tab.doc_elements(lambda: tab.display_chart(tab.plot_doc_countries()))
@@ -39,13 +21,17 @@ def task3b_widgets(tab):
     tab.set_on_open(lambda: tab.display_chart(tab.plot_browser_counts(True)))
 
 
-def task4d_widgets(tab):
-    pass
+def task4_widgets(tab):
+    tab.n_only(tab.display_reader_profiles)
+    tab.set_on_open(tab.display_reader_profiles)
 
-
-def task5_widgets(tab):
-    pass
+def task5d_widgets(tab):
+    tab.doc_elements(tab.display_also_likes_text)
+    tab.user_elements(row=1)
+    tab.set_on_open(tab.display_also_likes_text)
 
 
 def task6_widgets(tab):
-    pass
+    tab.doc_elements(tab.display_also_likes_graph)
+    tab.user_elements(row=1)
+    tab.set_on_open(tab.display_also_likes_graph)
